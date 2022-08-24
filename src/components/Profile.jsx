2ab4f3ec0396b96d39e2a93c46327da7686d88a7
Tanmay Tete <tanmay.tete@somaiya.edu>
2022-08-24 14:38:37 +0530

    better modified form

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import {useState} from "react";
-import {login} from "../services/auth";
+import { useState } from "react";
+import { login } from "../services/auth";
 
 
 const LoginForm = (props) => {
@@ -14,12 +14,21 @@ const LoginForm = (props) => {
         }, e => console.log(e))
     }
     return (
-        <div className="user-login">
-            <input type="text" value={username} id="login-username" onChange={e => setUsername(e.target.value)}/>
-            <input type="password" value={password} id="login-password" onChange={e => setPassword(e.target.value)}/>
-            <input type="button" value="login"
-                   onClick={signIn}/>
-            <input type="button" value="Sign up" onClick={() => props.switchToSignUp()}/>
+        <div className="container d-flex align-items-center justify-content-center">
+            <div id="login-form" className="user-login d-flex flex-column justify-content-center align-items-center">
+                <div className="mb-3">
+                    <label for="exampleInputEmail1" className="form-label">Email address</label>
+                    <input value={username} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onChange={e => setUsername(e.target.value)} />
+                </div>
+                <div className="mb-3">
+                    <label for="exampleInputPassword1" className="form-label">Password</label>
+                    <input value={password} type="password" className="form-control" id="exampleInputPassword1" onChange={e => setPassword(e.target.value)} />
+                </div>
+                <button onClick={signIn} type="submit" className="btn btn-primary mb-2">Submit</button>
+                <p>Not a user?
+                    <input className="boderless-button" type="button" value="Sign up" onClick={props.switchToSignUp} />
+                </p>
+            </div>
         </div>
     )
 }
@@ -27,8 +36,21 @@ const LoginForm = (props) => {
 const SignupForm = (props) => {
     return (
         <div className="user-signup">
-            Sign up form
-            <input type="button" value="Sign up" onClick={() => props.switchToLogIn()}/>
+            <div className="mb-3">
+                <label for="exampleInputEmail1" className="form-label">Email address</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div className="mb-3">
+                <label for="exampleInputPassword1" className="form-label">Password</label>
+                <input type="password" className="form-control" id="exampleInputPassword1" />
+            </div>
+            <div className="mb-3 form-check">
+                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                <label className="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+            <input type="button" value="login" onClick={() => props.switchToLogIn()} />
         </div>
     )
 }
@@ -38,7 +60,7 @@ const Profile = (props) => {
 
     const [signedUp, setSignedUp] = useState(true);
 
-    const {user, logout} = props;
+    const { user, logout } = props;
 
     if (props.user) {
         return (
@@ -47,15 +69,15 @@ const Profile = (props) => {
                     {user.name}
                 </div>
                 <div className="logout-button">
-                    <input type="button" value="Logout" onClick={logout}/>
+                    <input type="button" value="Logout" onClick={logout} />
                 </div>
             </div>
         )
     } else {
         if (signedUp)
-            return <LoginForm updateUserDeets={props.updateUserDeets} switchToSignUp={() => setSignedUp(false)}/>
+            return <LoginForm updateUserDeets={props.updateUserDeets} switchToSignUp={() => setSignedUp(false)} />
         else
-            return <SignupForm switchToLogIn={() => setSignedUp(true)}/>
+            return <SignupForm switchToLogIn={() => setSignedUp(true)} />
 
     }
 }
